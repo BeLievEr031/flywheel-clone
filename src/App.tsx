a2196f9d12b2eacafbe4b0441cd7ce611fab6216
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-25 15:06:20 +0530

    feat: features section responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import heroSrc from './assets/hero.png'
 import ContactUs from "./components/ContactUs"
+import Features from "./components/Features"
 function App() {
   return (
     <div className="min-h-screen px-10 md:px-20">
@@ -25,27 +26,7 @@ function App() {
 
         </div>
       </section>
-
-      <section className="grid grid-cols-3 pt-16 gap-4">
-
-        <div className="border-r-2 px-4 text-center border-r-black/60 text-text flex flex-col items-center">
-          <h1 className="uppercase font-fjalla-one text-5xl w-[223px]">REMOTELY control MULTIPLE MACHINES OR SITES</h1>
-          <p className="font-libre-baskerville text-[16px] pt-6">Manage your operator workforce more effectively</p>
-        </div>
-
-        <div className="border-r-2 px-4 text-center border-r-black/60 text-text flex flex-col items-center">
-          <h1 className="uppercase font-fjalla-one text-5xl w-[223px]">INCREASED SAFETY AND PREDICTABLE COSTS</h1>
-          <p className="font-libre-baskerville text-[16px] pt-6">Flywheel AI removes blindspots for operators and drastically reduces chances of safety incidents</p>
-        </div>
-
-        <div className="px-4 text-center text-text flex flex-col items-center">
-          <h1 className="uppercase font-fjalla-one text-5xl w-[223px]">MONITOR OPERATOR PERFORMANCE</h1>
-          <p className="font-libre-baskerville text-[16px] pt-6">Convenience for supervisors and managers to stay on top of things from anywhere
-          </p>
-        </div>
-
-      </section>
-
+      <Features />
       <ContactUs />
     </div>
   )
---
+++ src/components/Features.tsx
@@ -0,0 +1,27 @@
+
+function Features() {
+    return (
+
+        <section className="md:grid md:grid-cols-3 pt-16 gap-4">
+
+            <div className="md:border-r-2 mt-4 md:mt-0 px-4 text-center border-r-black/60 text-text flex flex-col items-center">
+                <h1 className="uppercase font-fjalla-one text-5xl md:w-[223px]">REMOTELY control MULTIPLE MACHINES OR SITES</h1>
+                <p className="font-libre-baskerville text-[16px] pt-3 md:pt-6">Manage your operator workforce more effectively</p>
+            </div>
+
+            <div className="md:border-r-2 mt-10 md:mt-0 px-4 text-center border-r-black/60 text-text flex flex-col items-center">
+                <h1 className="uppercase font-fjalla-one text-5xl md:w-[223px]">INCREASED SAFETY AND PREDICTABLE COSTS</h1>
+                <p className="font-libre-baskerville text-[16px] pt-3 md:pt-6">Flywheel AI removes blindspots for operators and drastically reduces chances of safety incidents</p>
+            </div>
+
+            <div className="px-4 text-center mt-10 md:mt-0 text-text flex flex-col items-center">
+                <h1 className="uppercase font-fjalla-one text-5xl md:w-[223px]">MONITOR OPERATOR PERFORMANCE</h1>
+                <p className="font-libre-baskerville text-[16px] pt-3 md:pt-6">Convenience for supervisors and managers to stay on top of things from anywhere
+                </p>
+            </div>
+
+        </section>
+    )
+}
+
+export default Features
