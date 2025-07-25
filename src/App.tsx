b2ecbb2e0a6ae429f53f4457cffeff8901ffba2a
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-25 14:29:17 +0530

    feat: add feature section

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,26 @@ function App() {
 
         </div>
       </section>
+
+      <section className="grid grid-cols-3 pt-16 gap-4">
+
+        <div className="border-r-2 px-4 text-center border-r-black/60 text-text flex flex-col items-center">
+          <h1 className="uppercase font-fjalla-one text-5xl w-[223px]">REMOTELY control MULTIPLE MACHINES OR SITES</h1>
+          <p className="font-libre-baskerville text-[16px] pt-6">Manage your operator workforce more effectively</p>
+        </div>
+
+        <div className="border-r-2 px-4 text-center border-r-black/60 text-text flex flex-col items-center">
+          <h1 className="uppercase font-fjalla-one text-5xl w-[223px]">INCREASED SAFETY AND PREDICTABLE COSTS</h1>
+          <p className="font-libre-baskerville text-[16px] pt-6">Flywheel AI removes blindspots for operators and drastically reduces chances of safety incidents</p>
+        </div>
+
+        <div className="px-4 text-center text-text flex flex-col items-center">
+          <h1 className="uppercase font-fjalla-one text-5xl w-[223px]">MONITOR OPERATOR PERFORMANCE</h1>
+          <p className="font-libre-baskerville text-[16px] pt-6">Convenience for supervisors and managers to stay on top of things from anywhere
+          </p>
+        </div>
+
+      </section>
     </div>
   )
 }
